5149c7234fbb3bdcf76f6e7987ae808acb3cc98b
Ameya <anetfreak@gmail.com>
2022-02-14 13:37:29 -0800

    Adding a basic Response dto for the helloworld method.

--- src/main/java/com/beyondfoo/greeting/adapter/port/in/HelloController.java
+++ src/main/java/com/beyondfoo/greeting/adapter/port/in/HelloController.java
@@ -1,14 +1,19 @@
 package com.beyondfoo.greeting.adapter.port.in;
 
+import com.beyondfoo.greeting.adapter.port.out.HelloResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
-    public String helloWorld() {
-        return "Hello World from Spring Boot";
+    public @ResponseBody
+    HelloResponse helloWorld() {
+        HelloResponse response = new HelloResponse();
+        response.setMessage("Hello World from Spring Boot");
+        return response;
     }
 
     @RequestMapping("/hello")
---
+++ src/main/java/com/beyondfoo/greeting/adapter/port/out/HelloResponse.java
@@ -0,0 +1,14 @@
+package com.beyondfoo.greeting.adapter.port.out;
+
+public class HelloResponse {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
